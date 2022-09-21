0eedf1eb5393ee3317f3a70e92ffbe27caf8b8a3
Sam Tomashi <samtomashikayembi@yahoo.fr>
2022-09-21 12:26:19 +0200

    Implement main component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Card from "./Card";
+import Navbar from "./Navbar";
 
 function App() {
+
+  const items = [
+    {
+      itemName: "Shoes",
+      description: "black leather",
+      price: 2300
+    },
+    {
+      itemName: "trouser",
+      description: "Blue Jeans",
+      price: 1000
+    },
+    {
+      itemName: "TShirt",
+      description: "Raphlauren Polo",
+      price: 2300
+    },
+    {
+      itemName: "Shirt",
+      description: "White shirt",
+      price: 1000
+    }
+  ];
+
+  const cards = items.map((item, index)=> {
+    return <React.Fragment>
+              <div class="col-3" key={index}>
+                  <Card  title={item.itemName} description={item.description} price={item.price}>
+                    <a href="#">View Details</a>
+                  </Card>
+              </div>
+        </React.Fragment>
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Navbar/>
+       <div class="row">
+        {cards}
+      </div>
+      <div class="fixed-bottom">Mashwear LTD</div>
     </div>
   );
 }
