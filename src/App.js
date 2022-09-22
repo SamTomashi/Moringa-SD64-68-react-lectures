bea4c128c084017dc5e0cef8ea2dafce470e79e8
Sam Tomashi <samtomashikayembi@yahoo.fr>
2022-09-22 12:18:37 +0200

    Implement fox fider

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { myTodos, getNextId } from './todos'
+import React, {useState} from 'react';
+/*
+  Add todo: spread operator
+  Remove todo: array filter
+  Update todo: array map
+*/
 function App() {
+  const [image, setImage] = useState("https://randomfox.ca/images/98.jpg");
+  const [link, setLink] = useState("")
+  const [likes, setLikes] = useState(0)
+
+  const getFox = ()=> {
+    fetch('https://randomfox.ca/floof/')
+    .then((response)=>response.json())
+    .then(({image, link})=> {
+      setImage(image)
+      setLink(link)
+      setLikes(0)
+    }
+    )
+  }
+
+  const like = () => {
+    setLikes(likes + 1)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container text-center'>
+      <h3>Fox Findr</h3>
+      <button onClick={getFox} type="button" className="mx-3 btn btn-primary btn-sm">Find Fox</button>
+      <button onClick={like} type="button" className="mx-3 btn btn-secondary btn-sm">{likes} Likes</button>
+      <div className='mx-3 p-3'>
+      <a href={link} target="_blank"><img src={image} className="img-fluid" alt="Responsive image"></img></a>
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
