1ac7660f8563361859f543792cee9f12b46d0523
Sam Tomashi <samtomashikayembi@yahoo.fr>
2022-09-25 15:19:38 +0200

    fetch items from DB

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import Card from "./Card";
 import Navbar from "./Navbar";
 
 function App() {
+
+  // const [items, setItems] = useState([])
 
   const items = [
     {
@@ -27,9 +29,16 @@ function App() {
     }
   ];
 
+  // useEffect(()=> {
+  //   fetch('http://localhost:3004/items')
+  //   .then(response => response.json())
+  //   .then(data => setItems(data))
+  // }, [])
+
+
   const cards = items.map((item, index)=> {
-    return <React.Fragment>
-              <div class="col-3" key={index}>
+    return <React.Fragment key={index}>
+              <div className="col-3">
                   <Card  title={item.itemName} description={item.description} price={item.price}>
                     <a href="#">View Details</a>
                   </Card>
@@ -40,10 +49,10 @@ function App() {
   return (
     <div className="container">
       <Navbar/>
-       <div class="row">
+       <div className="row">
         {cards}
       </div>
-      <div class="fixed-bottom">Mashwear LTD</div>
+      <div className="fixed-bottom">Mashwear LTD</div>
     </div>
   );
 }
