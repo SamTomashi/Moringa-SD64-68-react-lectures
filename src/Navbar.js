d829337147d1eb44720fc07fe99242c3b6a0073a
Sam Tomashi <samtomashikayembi@yahoo.fr>
2022-09-28 00:28:41 +0200

    implement fixed navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Navbar(){
     return <React.Fragment>
-                    <nav className="navbar navbar-expand-lg bg-light">
+                    <nav className="navbar navbar-expand-lg bg-light fixed-top">
             <div className="container-fluid">
                 <a className="navbar-brand" href="#">Navbar</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
